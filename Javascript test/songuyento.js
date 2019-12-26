b46f615f75ef4fd6cc2a2067f25c0bf6914d6613
phanvietdung2101 <phanvietdung2101@gmail.com>
2019-12-26 13:10:59 +0700

    xong bai so nguyen to

---
+++ Javascript test/songuyento.js
@@ -0,0 +1,10 @@
+
+<script>
+var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+var text = "";
+var i;
+for (i = 0; i < cars.length; i++) {
+    text += cars[i] + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
+</script>
