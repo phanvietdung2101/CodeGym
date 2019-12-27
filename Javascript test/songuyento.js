51eb1841505ab3e42a8a966876353029209766db
phanvietdung2101 <phanvietdung2101@gmail.com>
2019-12-27 16:10:40 +0700

    update ngay 27/12

--- Javascript test/songuyento.js
+++ Javascript test/songuyento.js
@@ -1,10 +1,30 @@
 
-<script>
-var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
-var text = "";
-var i;
-for (i = 0; i < cars.length; i++) {
-    text += cars[i] + "<br>";
+function songuyento()
+{
+    //Bien kiem tra f
+    let f=true;
+    let n;
+    n=prompt("Nhap gia tri n:");
+    parseInt(n);
+    if(n < 2){
+        f=false;
+    }
+    else{
+        //Lap gia tri i tu 2 den n-1
+        let i;
+        parseInt(i);
+        for (i = 2; i <= n-1; i++){
+            if (n % i == 0) {
+                f = false;
+                break;
+            }
+        }
+    }
+    // Kiem tra gia tri f de xac dinh n
+    if (f == true){
+        alert("n la so nguyen to ");
+    }
+    else{
+        alert("n khong phai la so nguyen to")
+    }
 }
-document.getElementById("demo").innerHTML = text;
-</script>
